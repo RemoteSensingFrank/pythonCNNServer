76ed5b479c5dc733f871f75bed5f3cddadaf1559
Frank Wu <wuwei_cug@163.com>
2018-10-31 06:47:35 +0800

    实现了fastrcnn的识别功能，准确将其在服务器端实现

--- CNNServer.py
+++ CNNServer.py
@@ -12,7 +12,7 @@ import mnistLeNet
 app = Flask(__name__)
 m_predict = mnistLeNet.LeNetModel_io()
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/mnist', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         dataList = json.loads(request.data.decode('utf-8'))
@@ -25,6 +25,13 @@ def upload_file():
         file.close()
         full_path=os.getcwd()+'/ImageFiles/'+datestr+'.png'
         return str(m_predict.predict(full_path))
+    return render_template('draw.html')
+
+@app.route('/fastrcnn',method=['GET','POST'])
+def fastrcnn_recoginze():
+    if request.method == 'POST':
+        #读取图片将其保存到服务器，然后调用识别的代码进行识别，返回识别结果包括：
+        #1. 识别范围；2.识别类别名称；3.结果的可靠性
     return render_template('draw.html')
 
 
